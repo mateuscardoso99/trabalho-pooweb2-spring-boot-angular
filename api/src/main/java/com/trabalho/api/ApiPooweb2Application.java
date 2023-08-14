482bac1d24de9d5e7c27f966abb19b4d6b61ae09
Mateus Augusto Vargas Cardoso <mat358591@gmail.com>
2023-08-14 18:08:25 -0300

    .

---
+++ api/src/main/java/com/trabalho/api/ApiPooweb2Application.java
@@ -0,0 +1,13 @@
+package com.trabalho.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPooweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPooweb2Application.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/trabalho/api/ApiPooweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPooweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
