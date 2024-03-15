8393e8dedbc1704e34fd32af3f7a87cfc2f22242
Mateus Augusto Vargas Cardoso <mat358591@gmail.com>
2024-03-15 18:45:37 -0300

    .

--- flutter_app/lib/components/Mapa.dart
+++ flutter_app/lib/components/Mapa.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 
 class Mapa extends StatelessWidget {
-  const Mapa({super.key});
 
+  final double lat;
+  final double lng;
+  const Mapa({super.key, required this.lat, required this.lng});
+
+
+  @override
    Widget build(BuildContext context) {
     return FlutterMap(
         mapController: MapController(),
-        options: const MapOptions(),
+        options: MapOptions(
+          initialCenter: LatLng(lat, lng),
+          initialZoom: 9.2,
+        ),
         children: [
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
